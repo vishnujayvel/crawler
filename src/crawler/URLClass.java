c3acda1a24e806a24dbcef9bf6a06d6f2ab62676
Nivetha Singara Vadivelu <nivethavadivelu@gmail.com>
2013-05-14 17:24:05 +0530

    final

--- src/crawler/URLClass.java
+++ src/crawler/URLClass.java
@@ -25,21 +25,44 @@ import java.awt.event.*;
 import javax.swing.*;
 
 
-class SearchFrame extends JFrame implements ActionListener {
+class SearchFrame extends JFrame implements MouseListener {
     JButton buttons[];
     JPanel panelSearchFrame = new JPanel();
+    DefaultListModel dlm=new DefaultListModel();
+    JList jl;
+    JScrollPane jsp;
 
 
     public SearchFrame() {}
     
     @Override
-    public void actionPerformed(ActionEvent e){
-        String url = e.getActionCommand();
-        WebFrame aPage = new WebFrame(url);
+    
+
+  
+    public void mousePressed(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseExited(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent me) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
-
-
 public class URLClass extends SearchFrame{
     
     public static DB db = new DB();
@@ -71,29 +94,51 @@ public class URLClass extends SearchFrame{
             buttons = new JButton[n];
             int i = 0, x = 100, y = 600, offsetX = 0, offsetY = 0;
             
+            searchTerm = JOptionPane.showInputDialog("Enter the search term: ");
+            
             while( rs.next()){
                 String PageContent = rs.getString(2);
                 if( searchSQL(PageContent, searchTerm)){
                     System.out.println();
-                    buttons[i] = new JButton();
+                    /*buttons[i] = new JButton();
                     System.out.println(buttons[i]);
                     System.out.println(offsetX+" "+offsetY);
                     buttons[i].setLocation(200, 200);
                     buttons[i].setSize(100, 30);
                     buttons[i].setPreferredSize(new Dimension(400, 30));
                     buttons[i].setText(rs.getString(1));
-                    buttons[i].addActionListener(this);
-                    panelSearchFrame.add(buttons[i]);
+                    buttons[i].addActionListener(this);*/
+                    dlm.addElement(rs.getString(1));
+                    
+                    //panelSearchFrame.add(buttons[i]);
+             
+                    
                     offsetX += 50;
                     offsetY += 50;
                     i++;
                 }
+                    
             }
+            jl=new JList(dlm);
+                jl.setVisibleRowCount(20);
+                    jsp=new JScrollPane(jl);
+                    jl.addMouseListener(new MouseAdapter(){
+                        public void mouseClicked(MouseEvent e){
+        if(e.getClickCount()==2){
+        String url = jl.getSelectedValue().toString();
+        System.out.println(url);
+        WebFrame aPage = new WebFrame(url);
+        }
+    }
+                    });
+               
+                    panelSearchFrame.add(jsp);
+                
             add(panelSearchFrame);
             setSize(561,534);
             setResizable(true);
             setVisible(true);
-            setTitle("frame");
+            setTitle("URLs Crawled");
         }catch(Exception e){
             System.out.println("OOPS!!! "+ e.getMessage());
         }
